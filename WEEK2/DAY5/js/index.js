1f07e869b92c87d419d9484c2b924382645b248e
zhouxiaotian <1282347298@qq.com>
2017-08-06 10:38:36 +0800

    '表格排序-最简单版'

--- WEEK2/DAY5/js/index.js
+++ WEEK2/DAY5/js/index.js
@@ -29,3 +29,32 @@ var stuList = document.getElementById('stuList'),
     }
     stuBody.innerHTML = str;
 }();
+
+//->让所有的行按照年龄由小到大排序
+~function () {
+
+    function sortRows() {
+        //->把存储所有行的类数组转换为数组(只有数组才能用SORT方法进行排序)
+        var stuRowsAry = utils.toArray(stuRows);
+
+        //->让所有的行按照年龄这一列由小到大排序
+        stuRowsAry.sort(function (a, b) {
+            var curAge = parseFloat(a.cells[2].innerHTML);
+            var nextAge = parseFloat(b.cells[2].innerHTML);
+            return curAge - nextAge;
+        });
+
+        //->按照数组排好的顺序,我们把每一行重新的放入到页面中,让页面中的结构也跟着排序
+        var frg = document.createDocumentFragment();
+        for (var i = 0; i < stuRowsAry.length; i++) {
+            var curRow = stuRowsAry[i];
+            frg.appendChild(curRow);
+        }
+        stuBody.appendChild(frg);
+        frg = null;
+    }
+
+    sortRows();
+
+
+}();
